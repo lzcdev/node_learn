e66de2c303fbe23728d37bf958ef37608aa5273c
lzcdev <lzc2134@gmail.com>
2018-12-04 17:19:29 +0800

    Update

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var path = require('path')
+var mongoose = require('mongoose')
+var _ = require('underscore')
+var Movie = require('./models/movie')
 var port = process.env.PORT || 3000
 var app = express()
+
+mongoose.connect('mongodb://localhost/movie_web').then(() => {
+	console.log('Connected to Database');
+	}).catch((err) => {
+		console.log("Not Connected to Database Error ", err);
+});
+
 
 app.set('views', './views/pages')
 app.set('view engine', 'jade')
@@ -14,50 +24,27 @@ console.log('immoc started on port ' + port)
 
 // index page
 app.get('/', function(req, res) {
-	res.render('index', {
-		title: 'immoc 首页',
-		movies: [{
-			title: '机械战警',
-			_id: 1,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title: '机械战警',
-			_id: 2,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title: '机械战警',
-			_id: 3,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title: '机械战警',
-			_id: 4,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title: '机械战警',
-			_id: 5,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title: '机械战警',
-			_id: 6,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		}]
+
+	Movie.fetch(function(err, movies){
+		if (err) {
+			console.log(err)
+		}
+		res.render('index', {
+			title: 'immoc 首页',
+			movies: movies
+		})
 	})
 })
 
 // detail page
 app.get('/movie/:id', function(req, res) {
-	res.render('detail', {
-		title: 'immoc 详情页',
-		movies: [{
-			title: '机械战警',
-			doctor: '何塞帕迪利亚',
-			country: '美国',
-			year: 2014,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-			summary: '哈哈哈哈',
-			language: '英语'			
-		}]
+	var id = req.params.id
+
+	Movie.findById(id, function(err, movie){
+		res.render('detail', {
+			title: 'immoc ' + movie.title,
+			movies: movie
+		})
 	})
 })
 
@@ -78,20 +65,67 @@ app.get('/admin/movie', function(req, res) {
 	})
 })
 
+// admin update movie
+app.get('/admin/update/:id', function(req, res) {
+	var id = req.params.id
+
+	if (id) {
+		Movie.findById(id, function(err, movie) {
+			res.render('admin', {
+				title: 'imooc 后台更新页',
+				movie: movie
+			})
+		})
+	}
+})
+
+// admin post movie
+app.post('/amdin/movie/new', function(res, req) {
+	var id = req.body.movie._id
+	var movieObj = req.body.movie
+	var _movie
+
+	if (id !== 'undefined') {
+		Movie.findById(id, function(err, movie) {
+			if (err) {console.log(err)}
+
+			_movie = _.extend(movie, movieObj)
+			_movie.save(function(err, movie){
+				if (err) {console.log(err)}
+
+				res.redirect('/movie/' + movie._id)
+			})
+		})
+	}
+	else {
+		_movie = new Movie({
+			doctor: movieObj.doctor,
+			title: movieObj.title,
+			country: movieObj.country,
+			language: movieObj.language,
+			year: movieObj.year,
+			poster: movieObj.poster,
+			summary: movieObj.summary,
+			flash: movieObj.flash,
+		})
+		_movie.save(function(err, movie) {
+			if (err) {console.log(err)}
+
+			res.redirect('/movie/' + movie._id)
+		})
+	}
+})
+
 
 // list page
 app.get('/admin/list', function(req, res) {
-	res.render('list', {
-		title: 'immoc 列表页',
-		movies: [{
-			title: '机械战警',
-			_id: 1,
-			doctor: '何塞帕迪利亚',
-			country: '美国',
-			year: 2014,
-			language: '英语',
-			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-			summary: '反派啊和技术开发环境啊啥的饭卡的时刻发动机哈电视就看韩剧卡收到啦好久好久啊还是飞机卡号发哈就上飞机啊书法家卡手机壳拉萨河看见了哈哈的 '
-		}]
+	Movie.fetch(function(err, movies) {
+		if (err) {
+			console.log(err)
+		}
+		res.render('list', {
+			title: 'immoc 列表页',
+			movies: movies
+		})
 	})
 })
