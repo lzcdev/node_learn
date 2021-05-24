07307df702a12d875550fd157160b61785be82c5
lzcdev <lzc2134@gmail.com>
2021-05-24 20:15:59 +0800

    docs: commonjs 模块化

---
+++ node_develop_actual/2.commonjs/index.js
@@ -0,0 +1,20 @@
+const game = require("./lib");
+
+var playerAction = process.argv[process.argv.length - 1];
+
+// const result = game(playerAction);
+// console.log(result);
+
+let count = 0
+process.stdin.on("data", (e) => {
+  const playerAction = e.toString().trim();
+
+  const result = game(playerAction);
+  if(result === -1) {
+    count++
+  }
+  if(count === 3) {
+    console.log('你太厉害了，不玩了')
+      process.exit()
+  }
+});
