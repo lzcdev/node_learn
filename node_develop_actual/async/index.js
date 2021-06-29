72e15b9d0651bbd7519107cc529116eebfce5f9e
lzcdev <lzc2134@gmail.com>
2021-06-29 20:16:49 +0800

    feat: async/await

---
+++ node_develop_actual/async/index.js
@@ -0,0 +1,20 @@
+/**
+ * async\await
+ * async function 是 Promise 的语法糖封装
+ * 异步编程的终极解决方案-以同步的方式写异步
+ */
+
+// const a = (async function () {
+//   var content = await new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(6);
+//     }, 500);
+//   });
+//   console.log(content);
+//   return 1
+// })();
+
+// setTimeout(() => {
+//   console.log(a);
+// }, 800);
+
